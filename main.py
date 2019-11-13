523bdfa768971caa8ca876cb4e191241a9769013
Sam Zagrobelny <strangesast@gmail.com>
2019-11-13 11:34:20 -0500

    first

---
+++ main.py
@@ -0,0 +1,56 @@
+"""
+Based on quickstart example at https://developers.google.com/sheets/api/quickstart/python
+
+"""
+
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets'] # read / write
+
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+RANGE = 'Class Data!A2:E'
+
+
+def get_creds(creds_filename = 'credentials.json', token_filename = 'token.pickle'):
+    creds = None
+    if os.path.exists(token_filename):
+        with open(token_filename, 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(creds_filename, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(token_filename, 'wb') as token:
+            pickle.dump(creds, token)
+
+    return creds
+
+
+def main():
+    creds = get_creds()
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=RANGE).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+
+if __name__ == '__main__':
+    main()
